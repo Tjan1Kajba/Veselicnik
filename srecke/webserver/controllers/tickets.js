5fb74f9e677a8a1ba59ff56e1863cdef9f42934a
mare555 <80032554+mare555@users.noreply.github.com>
2026-01-02 20:02:51 +0100

    send logs to rabbit mqtt in service: srecke

--- srecke/webserver/controllers/tickets.js
+++ srecke/webserver/controllers/tickets.js
@@ -1,11 +1,34 @@
 const Ticket = require("../models/Ticket");
 const axios = require("axios");
 
+const { sendLog } = require("../logger/rabbitmq.js");
+
 exports.createTicket = async (req, res) => {
   try {
     const ticket = await Ticket.create(req.body);
+    // throw new Error("Simulated failure");
+
+    // Send log to RabbitMQ
+    await sendLog(
+      "INFO",
+      req.originalUrl,
+      req.method,
+      true,
+      "",
+      req.correlationId
+    );
+
     res.status(201).json(ticket);
   } catch (err) {
+    // Send log to RabbitMQ
+    await sendLog(
+      "INFO",
+      req.originalUrl,
+      req.method,
+      false,
+      "",
+      req.correlationId
+    );
     res.status(400).json({ error: err.message });
   }
 };
@@ -14,6 +37,17 @@ exports.createTicketAndMusicRequest = async (req, res) => {
   const { userId, songName, artist } = req.body;
 
   if (!userId || !songName || !artist) {
+
+    // Send log to RabbitMQ
+    await sendLog(
+      "INFO",
+      req.originalUrl,
+      req.method,
+      false,
+      "",
+      req.correlationId
+    );
+
     return res.status(400).json({ error: "Missing required fields" });
   }
 
@@ -32,6 +66,17 @@ exports.createTicketAndMusicRequest = async (req, res) => {
 
     const response = await axios.post("http://music-service:8000/music/requests", musicRequest);
 
+
+    // Send log to RabbitMQ
+    await sendLog(
+      "INFO",
+      req.originalUrl,
+      req.method,
+      true,
+      "",
+      req.correlationId
+    );
+
     // Return combined response
     res.status(201).json({
       ticket,
@@ -39,12 +84,34 @@ exports.createTicketAndMusicRequest = async (req, res) => {
     });
   } catch (err) {
     console.error(err);
+
+    // Send log to RabbitMQ
+    await sendLog(
+      "INFO",
+      req.originalUrl,
+      req.method,
+      false,
+      "",
+      req.correlationId
+    );
+
     res.status(500).json({ error: "Internal server error" });
   }
 };
 
 exports.getTickets = async (req, res) => {
   const tickets = await Ticket.find();
+
+  // Send log to RabbitMQ
+  await sendLog(
+    "INFO",
+    req.originalUrl,
+    req.method,
+    true,
+    "",
+    req.correlationId
+  );
+
   res.json(tickets);
 };
 
@@ -55,15 +122,75 @@ exports.updateTicket = async (req, res) => {
       req.body,
       { new: true }
     );
-    if (!updated) return res.status(404).json({ error: "Ticket not found" });
+    if (!updated) {
+
+      // Send log to RabbitMQ
+      await sendLog(
+        "INFO",
+        req.originalUrl,
+        req.method,
+        false,
+        "",
+        req.correlationId
+      );
+      return res.status(404).json({ error: "Ticket not found" });
+    }
+
+
+    // Send log to RabbitMQ
+    await sendLog(
+      "INFO",
+      req.originalUrl,
+      req.method,
+      true,
+      "",
+      req.correlationId
+    );
+
     res.json(updated);
   } catch (err) {
+
+    // Send log to RabbitMQ
+    await sendLog(
+      "INFO",
+      req.originalUrl,
+      req.method,
+      false,
+      "",
+      req.correlationId
+    );
+
     res.status(400).json({ error: err.message });
   }
 };
 
 exports.deleteTicket = async (req, res) => {
   const deleted = await Ticket.findByIdAndDelete(req.params.id);
-  if (!deleted) return res.status(404).json({ error: "Ticket not found" });
+
+  if (!deleted) {
+
+    // Send log to RabbitMQ
+    await sendLog(
+      "INFO",
+      req.originalUrl,
+      req.method,
+      false,
+      "",
+      req.correlationId
+    );
+
+    return res.status(404).json({ error: "Ticket not found" });
+  }
+
+  // Send log to RabbitMQ
+  await sendLog(
+    "INFO",
+    req.originalUrl,
+    req.method,
+    true,
+    "",
+    req.correlationId
+  );
+
   res.status(204).send();
 };
